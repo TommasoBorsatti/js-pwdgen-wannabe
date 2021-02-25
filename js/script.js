e0186983d47832c63e773407060dca7e380260a3
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-02-25 14:17:14 +0100

    Esercizio svolto
    Essendo corto, non c'è stato bisogno di commit intermedi

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+var name = prompt("QUAL'E' IL TUO NOME ???");
+
+var lastName = prompt("QUAL'E' IL TUO COGNOME ???");
+
+var color = prompt("QUAL'E' IL TUO COLORE PREFERITO ???");
+
+document.getElementById("pw").innerHTML = name + lastName + color ;
